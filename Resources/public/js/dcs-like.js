05e0cbde6dafe1060b73d2d584c5d52fb561292e
Damiano Ciarla <damiano.ciarla@gmail.com>
2015-03-13 11:32:15 +0100

    prevent bubble

--- Resources/public/js/dcs-like.js
+++ Resources/public/js/dcs-like.js
@@ -1,6 +1,7 @@
 $(function () {
-    $(document).on('click', '.dcs-like-container a', function (event) {
+    $(document).on('click', '.dcs-like-container a:not(.disabled)', function (event) {
         event.preventDefault();
+        $(this).addClass('disabled');
         var element = $(this);
         $.get(element.attr('href'), function (data) {
             element.parents('.dcs-like-container').html(data);
