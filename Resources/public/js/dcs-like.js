9ac67e315da8626d7caf6ecb5f3aabf2500f244f
Damiano Ciarla <damiano.ciarla@gmail.com>
2015-06-09 09:44:34 +0200

    Update js

--- Resources/public/js/dcs-like.js
+++ Resources/public/js/dcs-like.js
@@ -4,7 +4,8 @@ $(function () {
         $(this).addClass('disabled');
         var element = $(this);
         $.get(element.attr('href'), function (data) {
-            element.parents('.dcs-like-container').html(data);
+            var threadId = element.parents('.dcs-like-container').data('dcs-like-thread-id');
+            $('.dcs-like-container[data-dcs-like-thread-id="'+threadId+'"]').html(data);
         });
     });
 });
